86a4a30ac8da036ce31d1326b94cc9703d61cc2c
Lakshmi1184 <42659837+Lakshmi1184@users.noreply.github.com>
2018-10-30 20:58:16 -0600

    Add files via upload

---
+++ Project3ExceptionHandling/CustomException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*namespace Project3ExceptionHandling
+{
+      class   CustomException:Exception
+    {
+        public CustomException()
+        {
+        }
+        public CustomException(string messgae):base(messgae)
+
+        {
+
+
+        }
+
+        /*public  CustomException (string message,Exception innerException):base(message,innerException)
+        {
+
+
+        }
+            
+            
+            
+            }
+
+           
+}
+*/
---
+++ Project3ExceptionHandling/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Project3ExceptionHandling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lbNumber1 = new System.Windows.Forms.Label();
+            this.lbNumber2 = new System.Windows.Forms.Label();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.btndivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubstract = new System.Windows.Forms.Button();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(172, 304);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(95, 28);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lbNumber1
+            // 
+            this.lbNumber1.AutoSize = true;
+            this.lbNumber1.Location = new System.Drawing.Point(136, 72);
+            this.lbNumber1.Name = "lbNumber1";
+            this.lbNumber1.Size = new System.Drawing.Size(74, 20);
+            this.lbNumber1.TabIndex = 1;
+            this.lbNumber1.Text = "Number1";
+            // 
+            // lbNumber2
+            // 
+            this.lbNumber2.AutoSize = true;
+            this.lbNumber2.Location = new System.Drawing.Point(136, 143);
+            this.lbNumber2.Name = "lbNumber2";
+            this.lbNumber2.Size = new System.Drawing.Size(74, 20);
+            this.lbNumber2.TabIndex = 2;
+            this.lbNumber2.Text = "Number2";
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(328, 65);
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 26);
+            this.txtNumber1.TabIndex = 3;
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(328, 137);
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 26);
+            this.txtNumber2.TabIndex = 4;
+            // 
+            // btndivide
+            // 
+            this.btndivide.Location = new System.Drawing.Point(556, 298);
+            this.btndivide.Name = "btndivide";
+            this.btndivide.Size = new System.Drawing.Size(120, 34);
+            this.btndivide.TabIndex = 5;
+            this.btndivide.Text = "Divide";
+            this.btndivide.UseVisualStyleBackColor = true;
+            this.btndivide.Click += new System.EventHandler(this.btndivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(444, 298);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(77, 34);
+            this.btnMultiply.TabIndex = 6;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnSubstract
+            // 
+            this.btnSubstract.Location = new System.Drawing.Point(309, 304);
+            this.btnSubstract.Name = "btnSubstract";
+            this.btnSubstract.Size = new System.Drawing.Size(90, 28);
+            this.btnSubstract.TabIndex = 7;
+            this.btnSubstract.Text = "Substract";
+            this.btnSubstract.UseVisualStyleBackColor = true;
+            this.btnSubstract.Click += new System.EventHandler(this.btnSubstract_Click);
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(136, 205);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(55, 20);
+            this.lbResult.TabIndex = 8;
+            this.lbResult.Text = "Result";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(328, 218);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(245, 26);
+            this.txtResult.TabIndex = 9;
+            this.txtResult.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.btnSubstract);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btndivide);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Controls.Add(this.lbNumber2);
+            this.Controls.Add(this.lbNumber1);
+            this.Controls.Add(this.btnAdd);
+            this.Name = "Form1";
+            this.Text = "Alice Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lbNumber1;
+        private System.Windows.Forms.Label lbNumber2;
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.Button btndivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubstract;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.TextBox txtResult;
+    }
+}
+
